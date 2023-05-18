b635e98d6476721888b384955c3ccaf864078f5e
Zhanghao <yukiniro@hotmail.com>
2023-05-18 18:02:15 +0800

    feat: add canvas benchmark

--- tinybench/index.mjs
+++ tinybench/index.mjs
@@ -1,16 +1,32 @@
 import { Bench } from "tinybench";
+import { createCanvas } from "canvas";
 
 const bench = new Bench({ time: 100 });
+const canvas1 = createCanvas(500, 500);
+const canvas2 = createCanvas(500, 500);
+const canvas3 = createCanvas(500, 500);
+
+function rederBK(canvas) {
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#F00";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 bench
-  .add("faster task", () => {
-    console.log("I am faster");
+  .add("size", () => {
+    rederBK(canvas1);
+    canvas1.width = canvas1.width;
+    canvas1.height = canvas1.height;
   })
-  .add("slower task", async () => {
-    await new Promise((r) => setTimeout(r, 1)); // we wait 1ms :)
-    console.log("I am slower");
+  .add("fillRect", async () => {
+    rederBK(canvas2);
+    canvas2.getContext("2d").fillStyle = "#FFF";
+    canvas2.getContext("2d").fillRect(0, 0, canvas2.width, canvas2.height);
   })
-  .todo("unimplemented bench");
+  .add("clearRect", async () => {
+    rederBK(canvas3);
+    canvas3.getContext("2d").clearRect(0, 0, canvas3.width, canvas3.height);
+  });
 
 await bench.run();
 
